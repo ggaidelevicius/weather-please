a42150839c33f22f370a44b998839aca9e14a813
Gus Gaidelevicius <ggaidelevicius@gmail.com>
2023-08-22 13:17:14 +0800

    chore: update bundle identifier

--- platforms/apple/Shared (App)/ViewController.swift
+++ platforms/apple/Shared (App)/ViewController.swift
@@ -16,7 +16,7 @@ import SafariServices
 typealias PlatformViewController = NSViewController
 #endif
 
-let extensionBundleIdentifier = "com.yourCompany.Weather-Please.Extension"
+let extensionBundleIdentifier = "com.gusGaidelevicius.Weather-Please.Extension"
 
 class ViewController: PlatformViewController, WKNavigationDelegate, WKScriptMessageHandler {
 
