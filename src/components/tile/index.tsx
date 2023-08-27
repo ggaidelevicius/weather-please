0ce3c9c8734c141d2531f19496a57468d77e1f59
ggaidelevicius <62304030+ggaidelevicius@users.noreply.github.com>
2023-08-27 20:23:18 +0800

    feat: show date on tile hover

--- src/components/tile/index.tsx
+++ src/components/tile/index.tsx
@@ -1,17 +1,37 @@
 import { Card, Title } from '@mantine/core'
+import classnames from 'classnames'
 import type { FC } from 'react'
+import { useState } from 'react'
 import styles from './styles.module.css'
-import type { Days, TileProps } from './types'
+import type { Days, Months, TileProps } from './types'
 import { BasicWeather, WeatherDetail } from './weather'
 
 const Tile: FC<TileProps> = (props: TileProps) => {
   const { day } = props
+  const [hovering, setHovering] = useState<boolean>(false)
   const days: Days[] = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+  const months: Months[] = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
 
   return (
-    <Card component="article" padding="lg" radius="md" sx={{ userSelect: 'none' }}>
-      <Title order={2} className={styles.title}>
-        {days[new Date(day * 1000).getDay()]}
+    <Card
+      component="article"
+      padding="lg"
+      radius="md"
+      sx={{ userSelect: 'none' }}
+      onMouseEnter={() => { setHovering(true) }}
+      onMouseLeave={() => { setHovering(false) }}
+    >
+      <Title
+        order={2}
+        className={styles.title}
+        aria-label={`${days[new Date(day * 1000).getDay()]}, ${new Date(day * 1000).getDate()} ${months[new Date(day * 1000).getMonth()]}`}
+      >
+        <span aria-hidden className={classnames(styles.day, hovering ? styles.dayHover : undefined)}>
+          {days[new Date(day * 1000).getDay()]}
+        </span>
+        <span aria-hidden className={classnames(styles.date, hovering ? styles.dateHover : undefined)}>
+          {`${new Date(day * 1000).getDate()} ${months[new Date(day * 1000).getMonth()]}`}
+        </span>
       </Title>
       <BasicWeather {...props} />
       <WeatherDetail {...props} />
--- src/components/tile/types.d.ts
+++ src/components/tile/types.d.ts
@@ -12,3 +12,5 @@ export interface TileProps {
 }
 
 export type Days = 'Sunday' | 'Monday' | 'Tuesday' | 'Wednesday' | 'Thursday' | 'Friday' | 'Saturday'
+
+export type Months = 'January' | 'February' | 'March' | 'April' | 'May' | 'June' | 'July' | 'August' | 'September' | 'October' | 'November' | 'December'
