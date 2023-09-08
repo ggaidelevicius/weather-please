774a600d335a4f9c7999d8cae0f4715d7c6abd02
ggaidelevicius <62304030+ggaidelevicius@users.noreply.github.com>
2023-09-08 23:11:49 +0800

    feat: bengali and indonesian translations

--- lingui.config.js
+++ lingui.config.js
@@ -1,6 +1,6 @@
 /** @type {import('@lingui/conf').LinguiConfig} */
 module.exports = {
-  locales: ['en', 'lt', 'vi', 'hi', 'zh', 'ja', 'es', 'de', 'fr', 'it', 'ko', 'ru'],
+  locales: ['en', 'lt', 'vi', 'hi', 'zh', 'ja', 'es', 'de', 'fr', 'it', 'ko', 'ru', 'bn', 'id'],
   catalogs: [
     {
       path: '<rootDir>/src/locales/{locale}/messages',
--- src/util/i18n.ts
+++ src/util/i18n.ts
@@ -1,11 +1,13 @@
 import { i18n } from '@lingui/core'
 
 export const locales: Record<string, string> = {
+  bn: 'বাংলা', // bengali
   de: 'Deutsch', // german
   en: 'English', // english
   es: 'Español', // spanish
   fr: 'Français', // french
   hi: 'हिंदी', // hindi
+  id: 'Bahasa Indonesia', // indonesian
   it: 'Italiano', // italian
   ja: '日本語', // japanese
   ko: '한국인', // korean
