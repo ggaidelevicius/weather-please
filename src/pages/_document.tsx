55d1c2d9ce9d6f51464b5c2c4e7c4da4b251e3ba
ggaidelevicius <62304030+ggaidelevicius@users.noreply.github.com>
2023-09-23 22:51:03 +0800

    fix: remove inline script // extensions complain, can't allow in manifest

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,12 +1,9 @@
-import { ColorSchemeScript } from '@mantine/core'
 import { Head, Html, Main, NextScript } from 'next/document'
 
 const Document = () => {
   return (
     <Html lang='en'>
-      <Head>
-        <ColorSchemeScript defaultColorScheme='dark' />
-      </Head>
+      <Head />
       <body>
         <Main />
         <NextScript />
