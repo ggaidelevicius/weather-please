e5402c01b07d7eec18e68b7ef7f7be3558b6045d
ggaidelevicius <62304030+ggaidelevicius@users.noreply.github.com>
2023-09-29 18:33:38 +0800

    chore: add title and noindex

--- src/pages/feedback/index.tsx
+++ src/pages/feedback/index.tsx
@@ -22,6 +22,7 @@ import {
 	IconMessageCircleQuestion,
 } from '@tabler/icons-react'
 import { AnimatePresence, motion } from 'framer-motion'
+import Head from 'next/head'
 import { useRouter } from 'next/router'
 import { useEffect, useState } from 'react'
 
@@ -78,143 +79,149 @@ const Feedback = () => {
 
 	if (process.env.NEXT_PUBLIC_DEMO === 'true') {
 		return (
-			<main>
-				<AnimatePresence>
-					{pageLoaded && (
-						<motion.form
-							initial={{ scale: 0.95, opacity: 0 }}
-							animate={{
-								scale: 1,
-								opacity: 1,
-								transition: {
-									type: 'spring',
-									duration: 1.5,
-									delay: 0.075,
-								},
+			<>
+			<Head>
+				<title>Weather Please Feedback</title>
+				<meta name="robots" content="noindex" />
+			</Head>
+				<main>
+					<AnimatePresence>
+						{pageLoaded && (
+							<motion.form
+								initial={{ scale: 0.95, opacity: 0 }}
+								animate={{
+									scale: 1,
+									opacity: 1,
+									transition: {
+										type: 'spring',
+										duration: 1.5,
+										delay: 0.075,
+									},
+								}}
+							>
+								<SegmentedControl
+									value={feedbackType}
+									onChange={setFeedbackType}
+									disabled={completed}
+									data={[
+										{
+											value: 'feedback',
+											label: (
+												<Center style={{ padding: '0 1rem' }}>
+													<IconMessageCircle
+														color="var(--mantine-color-teal-filled)"
+														aria-hidden
+														style={{ width: rem(32), height: rem(32) }}
+													/>
+													<Box ml={10}>
+														<Trans>Feedback</Trans>
+													</Box>
+												</Center>
+											),
+										},
+										{
+											value: 'feature',
+											label: (
+												<Center style={{ padding: '0 1rem' }}>
+													<IconMessageCircleQuestion
+														color="var(--mantine-color-blue-filled)"
+														aria-hidden
+														style={{ width: rem(32), height: rem(32) }}
+													/>
+													<Box ml={10}>
+														<Trans>Feature request</Trans>
+													</Box>
+												</Center>
+											),
+										},
+										{
+											value: 'bug',
+											label: (
+												<Center style={{ padding: '0 1rem' }}>
+													<IconMessageCircleExclamation
+														color="var(--mantine-color-red-filled)"
+														aria-hidden
+														style={{ width: rem(32), height: rem(32) }}
+													/>
+													<Box ml={10}>
+														<Trans>Bug report</Trans>
+													</Box>
+												</Center>
+											),
+										},
+									]}
+								/>
+								<Textarea
+									label={<Trans>Message</Trans>}
+									withAsterisk
+									autosize
+									minRows={4}
+									value={textareaValue}
+									onChange={(e) => setTextareaValue(e.currentTarget.value)}
+									mt="lg"
+									disabled={completed}
+								/>
+								<TextInput
+									mt="sm"
+									label={<Trans>Email (optional)</Trans>}
+									value={emailValue}
+									onChange={(e) => setEmailValue(e.currentTarget.value)}
+									type="email"
+									leftSectionPointerEvents="none"
+									leftSection={<IconAt size={16} />}
+									disabled={completed}
+								/>
+								<Button
+									mt="lg"
+									fullWidth
+									disabled={textareaValue === '' || completed}
+									onClick={handleClick}
+									loading={loading}
+								>
+									<Trans>Submit</Trans>
+								</Button>
+							</motion.form>
+						)}
+					</AnimatePresence>
+					{completed && (
+						<Card
+							mt="lg"
+							style={{
+								display: 'flex',
+								flexDirection: 'row',
+								alignItems: 'center',
+								justifyContent: 'center',
+								gap: '0.5rem',
 							}}
 						>
-							<SegmentedControl
-								value={feedbackType}
-								onChange={setFeedbackType}
-								disabled={completed}
-								data={[
-									{
-										value: 'feedback',
-										label: (
-											<Center style={{ padding: '0 1rem' }}>
-												<IconMessageCircle
-													color="var(--mantine-color-teal-filled)"
-													aria-hidden
-													style={{ width: rem(32), height: rem(32) }}
-												/>
-												<Box ml={10}>
-													<Trans>Feedback</Trans>
-												</Box>
-											</Center>
-										),
-									},
-									{
-										value: 'feature',
-										label: (
-											<Center style={{ padding: '0 1rem' }}>
-												<IconMessageCircleQuestion
-													color="var(--mantine-color-blue-filled)"
-													aria-hidden
-													style={{ width: rem(32), height: rem(32) }}
-												/>
-												<Box ml={10}>
-													<Trans>Feature request</Trans>
-												</Box>
-											</Center>
-										),
-									},
-									{
-										value: 'bug',
-										label: (
-											<Center style={{ padding: '0 1rem' }}>
-												<IconMessageCircleExclamation
-													color="var(--mantine-color-red-filled)"
-													aria-hidden
-													style={{ width: rem(32), height: rem(32) }}
-												/>
-												<Box ml={10}>
-													<Trans>Bug report</Trans>
-												</Box>
-											</Center>
-										),
-									},
-								]}
-							/>
-							<Textarea
-								label={<Trans>Message</Trans>}
-								withAsterisk
-								autosize
-								minRows={4}
-								value={textareaValue}
-								onChange={(e) => setTextareaValue(e.currentTarget.value)}
-								mt="lg"
-								disabled={completed}
-							/>
-							<TextInput
-								mt="sm"
-								label={<Trans>Email (optional)</Trans>}
-								value={emailValue}
-								onChange={(e) => setEmailValue(e.currentTarget.value)}
-								type="email"
-								leftSectionPointerEvents="none"
-								leftSection={<IconAt size={16} />}
-								disabled={completed}
-							/>
-							<Button
-								mt="lg"
-								fullWidth
-								disabled={textareaValue === '' || completed}
-								onClick={handleClick}
-								loading={loading}
-							>
-								<Trans>Submit</Trans>
-							</Button>
-						</motion.form>
+							<IconCircleCheckFilled size={30} />
+							<Text fz="lg" fw="bold">
+								<Trans>Feedback successfully submitted</Trans>
+							</Text>
+						</Card>
 					)}
-				</AnimatePresence>
-				{completed && (
-					<Card
-						mt="lg"
-						style={{
-							display: 'flex',
-							flexDirection: 'row',
-							alignItems: 'center',
-							justifyContent: 'center',
-							gap: '0.5rem',
-						}}
-					>
-						<IconCircleCheckFilled size={30} />
-						<Text fz="lg" fw="bold">
-							<Trans>Feedback successfully submitted</Trans>
-						</Text>
-					</Card>
-				)}
-				{error && (
-					<Card
-						mt="lg"
-						style={{
-							display: 'flex',
-							flexDirection: 'row',
-							alignItems: 'center',
-							justifyContent: 'center',
-							gap: '0.5rem',
-						}}
-					>
-						<IconAlertCircleFilled size={30} />
-						<Text fz="lg" fw="bold">
-							<Trans>Error submitting feedback. Try again later.</Trans>
-						</Text>
-					</Card>
-				)}
-			</main>
+					{error && (
+						<Card
+							mt="lg"
+							style={{
+								display: 'flex',
+								flexDirection: 'row',
+								alignItems: 'center',
+								justifyContent: 'center',
+								gap: '0.5rem',
+							}}
+						>
+							<IconAlertCircleFilled size={30} />
+							<Text fz="lg" fw="bold">
+								<Trans>Error submitting feedback. Try again later.</Trans>
+							</Text>
+						</Card>
+					)}
+				</main>
+			</>
 		)
 	}
-	return
+	return <></>
 }
 
 export default Feedback
