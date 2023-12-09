fb5b1a152484226284ad13ad5f761db6607a1cf2
ggaidelevicius <62304030+ggaidelevicius@users.noreply.github.com>
2023-12-09 21:13:54 +0800

    fix: correct button condition

--- src/pages/api/feedback/index.tsx
+++ src/pages/api/feedback/index.tsx
@@ -37,7 +37,7 @@ const Handler: NextApiHandler = async (req, res) => {
 				integerValue: installed,
 			},
 			reasons: {
-				mapValue: JSON.stringify(reasons),
+				stringValue: JSON.stringify(reasons),
 			},
 		},
 	}
