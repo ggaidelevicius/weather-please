63b5b68cf057917ffef3e213b720a117e35a6b29
ggaidelevicius <62304030+ggaidelevicius@users.noreply.github.com>
2023-12-09 09:52:49 +0800

    chore: deps bump

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,7 @@
 import '@/styles/styles.css'
 import { i18n } from '@lingui/core'
 import { I18nProvider } from '@lingui/react'
-import { MantineProvider } from '@mantine/core'
+import { MantineProvider, createTheme } from '@mantine/core'
 import '@mantine/core/styles.css'
 import { Notifications } from '@mantine/notifications'
 import '@mantine/notifications/styles.css'
@@ -9,6 +9,23 @@ import { Analytics } from '@vercel/analytics/react'
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
 import type { FC } from 'react'
+
+const theme = createTheme({
+	colors: {
+		dark: [
+			'#C1C2C5',
+			'#A6A7AB',
+			'#909296',
+			'#5c5f66',
+			'#373A40',
+			'#2C2E33',
+			'#25262b',
+			'#1A1B1E',
+			'#141517',
+			'#101113',
+		],
+	},
+})
 
 const App: FC<AppProps> = (props) => {
 	const { Component, pageProps } = props
@@ -23,7 +40,7 @@ const App: FC<AppProps> = (props) => {
 				/>
 				<link rel="icon" href="/favicon.png" />
 			</Head>
-			<MantineProvider defaultColorScheme="dark">
+			<MantineProvider defaultColorScheme="dark" theme={theme}>
 				<Notifications
 					position="top-right"
 					transitionDuration={1000}
