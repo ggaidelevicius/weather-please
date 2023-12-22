bf1fa9169f988432d716c3e6df8dad0f2cc2cc8d
ggaidelevicius <62304030+ggaidelevicius@users.noreply.github.com>
2023-12-22 11:04:57 +0800

    revert: token function abstraction

--- src/app/api/lib/token.ts
+++ src/app/api/lib/token.ts
@@ -1,13 +1,1 @@
-export const key = crypto.subtle.importKey(
-	'raw',
-	new TextEncoder().encode(process.env.OG_KEY),
-	{ name: 'HMAC', hash: { name: 'SHA-256' } },
-	false,
-	['sign'],
-)
 
-export const toHex = (arrayBuffer: ArrayBuffer) => {
-	return Array.prototype.map
-		.call(new Uint8Array(arrayBuffer), (n) => n.toString(16).padStart(2, '0'))
-		.join('')
-}
