fc13065b7b44ddb199a447f67eb375523baa827b
ggaidelevicius <62304030+ggaidelevicius@users.noreply.github.com>
2023-12-22 11:02:54 +0800

    refactor: token function abstraction

--- src/app/api/og/route.tsx
+++ src/app/api/og/route.tsx
@@ -1,21 +1,8 @@
 /* eslint-disable @next/next/no-img-element */
 import { ImageResponse } from 'next/og'
+import { key, toHex } from '../lib/token'
 
 export const runtime = 'edge'
-
-const key = crypto.subtle.importKey(
-  'raw',
-  new TextEncoder().encode(process.env.OG_KEY),
-  { name: 'HMAC', hash: { name: 'SHA-256' } },
-  false,
-  ['sign'],
-)
-
-export const toHex = (arrayBuffer: ArrayBuffer) => {
-  return Array.prototype.map
-    .call(new Uint8Array(arrayBuffer), (n) => n.toString(16).padStart(2, '0'))
-    .join('')
-}
 
 export const GET = async (request: Request) => {
   const { searchParams } = new URL(request.url)
