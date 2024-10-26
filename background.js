8b080b23a2db276703779d1279d8444b5ec07f3b
Gus Gaidelevicius <ggaidelevicius@gmail.com>
2024-10-26 13:37:05 +0800

    cleaner uninstall/install handling

--- background.js
+++ background.js
@@ -1,27 +1,28 @@
-chrome.runtime.onInstalled.addListener(() => {
-	chrome.storage.local.get(['installed']).then((res) => {
-		if (!res?.installed) {
-			chrome.storage.local.set({ installed: new Date().getTime() })
+chrome.runtime.onInstalled.addListener(async () => {
+	try {
+		const res = await chrome.storage.local.get(['installed'])
+		if (!res.installed) {
+			await chrome.storage.local.set({ installed: Date.now() })
 		}
-	})
+	} catch (error) {
+		// eslint-disable-next-line no-console
+		console.error('Error during onInstalled listener:', error)
+	}
 })
 
 const setUninstallURL = async () => {
-	const installed = await chrome.storage.local
-		.get(['installed'])
-		.then((res) => res?.installed ?? 0)
-	await chrome.runtime.setUninstallURL(
-		`https://weather-please.app/feedback?type=uninstall&installed=${installed}`,
-		() => {
-			if (chrome.runtime.lastError) {
-				// eslint-disable-next-line no-console
-				console.error('Error setting uninstall URL: ', chrome.runtime.lastError)
-			} else {
-				// eslint-disable-next-line no-console
-				console.log('Uninstall URL set successfully')
-			}
-		},
-	)
+	try {
+		const res = await chrome.storage.local.get(['installed'])
+		const installed = res.installed ?? 0
+		await chrome.runtime.setUninstallURL(
+			`https://weather-please.app/feedback?type=uninstall&installed=${installed}`,
+		)
+		// eslint-disable-next-line no-console
+		console.log('Uninstall URL set successfully')
+	} catch (error) {
+		// eslint-disable-next-line no-console
+		console.error('Error setting uninstall URL:', error)
+	}
 }
 
 setUninstallURL()
