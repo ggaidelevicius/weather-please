fc24558a92e8d3527214e05881bc4e500d0809bf
Gus Gaidelevicius <ggaidelevicius@gmail.com>
2025-03-04 17:13:56 +0800

    add analytics

--- src/app/bug/layout.tsx
+++ src/app/bug/layout.tsx
@@ -1,4 +1,5 @@
 import '@/styles/tailwind.css'
+import { Analytics } from '@vercel/analytics/react'
 import { type Metadata } from 'next'
 
 export const metadata: Metadata = {
@@ -17,6 +18,7 @@ const RootLayout = ({ children }: { children: React.ReactNode }) => {
 		<html lang="en" className='antialiased" h-full bg-dark-800'>
 			<body className="flex min-h-full flex-col items-center justify-center">
 				{children}
+				<Analytics />
 			</body>
 		</html>
 	)
