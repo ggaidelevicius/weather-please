f6afb7081f3c2b0f60f432f12c63f39b26da21ef
Gus Gaidelevicius <ggaidelevicius@gmail.com>
2025-03-04 20:15:19 +0800

    deps bump

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -4,7 +4,6 @@ const config: StorybookConfig = {
 	stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
 	addons: [
 		'@storybook/addon-essentials',
-		'@storybook/addon-onboarding',
 		'@chromatic-com/storybook',
 		'@storybook/experimental-addon-test',
 	],
