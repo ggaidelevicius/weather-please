1acd95540aa823e479ab456b57dad252e0ca07b6
Gus Gaidelevicius <ggaidelevicius@gmail.com>
2025-12-12 12:59:32 +0800

    remove swc macros

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,6 @@ import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
 	reactStrictMode: true,
-	experimental: {
-		swcPlugins: [['@lingui/swc-plugin', {}]],
-		// reactCompiler: true,
-	},
 	turbopack: {},
 }
 
