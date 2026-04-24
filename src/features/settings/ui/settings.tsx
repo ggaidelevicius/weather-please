ba190b9cd600f1aae3e497a1a0148fdf4e390a20
Gus Gaidelevicius <ggaidelevicius@gmail.com>
2026-04-24 22:15:07 +0800

    increase z index

--- src/features/settings/ui/settings.tsx
+++ src/features/settings/ui/settings.tsx
@@ -341,7 +341,7 @@ export const Settings = ({ handleChange, input }: Readonly<SettingsProps>) => {
 	return (
 		<>
 			<IconButton
-				className="fixed right-4 bottom-4 shadow-md"
+				className="fixed right-4 bottom-4 z-2 shadow-md"
 				icon={IconSettings}
 				onClick={() => {
 					setActiveSection('general')
